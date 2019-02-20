b9e35f98c09ce485b952a6318494db8e73a9b7e3
Rodrigo Sousa <rod_sousa@hotmail.com>
2019-02-20 20:26:37 +0000

    Improvements!

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 function toggleVisible(id) {    
 }
 
-function changeImage(id, number) {
-    images_path = {};
-    document.getElementById(id).src = images_path[number];
+function changeImage(id) {
+    images_path = ["Isabel.jpeg", "Rodrigo.jpg"];
+    object = document.getElementById(id)
+    
+    if (id == "profile_pic_1") {
+        object.src = images_path[0];
+
+        /* pixels: 138 x 170 */
+        object.style="border-radius: 75px;";
+        object.width="138";
+        object.height="170";
+
+    } else if (id == "profile_pic_2") {
+        object.src = images_path[1];
+
+        /* pixels: 127 x 174 */
+        object.style="border-radius: 75px;";
+        object.width="127";
+        object.height="174";
+
+    } else if (id == "profile_pic_3") {
+        object.src = images_path[2];
+    }
 }
