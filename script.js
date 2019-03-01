751233ae1376b01e31290306be4182293d132b8c
Rodrigo Sousa <rod_sousa@hotmail.com>
2019-03-01 15:32:53 +0000

    Changed name of background image

--- script.js
+++ script.js
@@ -9,26 +9,15 @@ function changeImage(id) {
     if (id == "profile_pic_1") {
         object.src = images_path[0];
 
-        /* pixels: 138 x 170 */
-        object.style="border-radius: 75px;";
-        object.width="138";
-        object.height="170";
-
+        
     } else if (id == "profile_pic_2") {
         object.src = images_path[1];
-
-        /* pixels: 135 x 174 */
-        object.style="border-radius: 75px;";
-        object.width="135";
-        object.height="174";
-
     } else if (id == "profile_pic_3") {
         object.src = images_path[2];
-
-        /* pixels: 138 x 170 */
-        object.style="border-radius: 75px;";
-        object.width="138";
-        object.height="170";
-
     }
+
+    /* pixels: 138 x 170 */
+    object.style="border-radius: 75px;";
+    object.width="138";
+    object.height="170";
 }
