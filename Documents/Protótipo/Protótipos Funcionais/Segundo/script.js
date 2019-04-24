5b9922d9fe0a8f044b3cbd16dd3c541c4f3a41f6
Rodrigo Sousa <rod_sousa@hotmail.com>
2019-04-24 22:52:17 +0100

    Done with script

--- Documents/Protótipo/Protótipos Funcionais/Segundo/script.js
+++ Documents/Protótipo/Protótipos Funcionais/Segundo/script.js
@@ -1,72 +1,58 @@
-var actual_screen;
-var template;
-var frame;
-var description_bar;
-var friendsgroup = [];
+var actual_screen
+var template
+var frame
+var description_bar
+var friendsgroup = []
 
-const fps = 60;
-var counter = 0;
+const fps = 60
+var counter = 0
 
 function changeScreen(canvas, new_screen) {
-	canvas.removeChild(actual_screen);
-	actual_screen = new_screen;
-	canvas.addChild(new_screen);
+	canvas.removeChild(actual_screen)
+	actual_screen = new_screen
+	canvas.addChild(new_screen)
 
-	canvas.removeChild(description_bar);
-	description_bar.text = new_screen.description;
+	canvas.removeChild(description_bar)
+	description_bar.text = new_screen.description
 	if (new_screen.description_show) {
-		canvas.addChild(description_bar);
+		canvas.addChild(description_bar)
 	}
 
-	canvas.removeChild(template);
+	canvas.removeChild(template)
 	if (new_screen.template) {
-		canvas.addChild(template);
+		canvas.addChild(template)
 	}
 }
 
 function loadCanvas() {
-	canvas = document.getElementById('workzone');
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+	canvas = document.getElementById('workzone')
+	canvas.width = window.innerWidth
+	canvas.height = window.innerHeight
 
 	var canvas = oCanvas.create({
 		canvas: '#workzone',
 		fps: fps
-	});
+	})
+	
+	var center = build_ellipse(canvas, [canvas.width / 2, canvas.height / 2], RADIUS_WORKZONE, white)
+	canvas.addChild(center)
 
-	var center = canvas.display
-		.ellipse({
-			x: canvas.width / 2,
-			y: canvas.height / 2,
-			radius: RADIUS_WORKZONE,
-			fill: '#FFFFFF'
-		})
-		.add();
-
-	description_bar = canvas.display
-		.text({
-			x: canvas.width / 2,
-			y: canvas.height / 2 - SIZE_SCREEN / 2 / 8 * 6,
-			origin: { x: 'center', y: 'center' },
-			family: '7Segments',
-			font: get_size_px(canvas, 19),
-			fill: white
-		})
-		.add();
+	description_bar = build_text(canvas, [canvas.width / 2, canvas.height / 2 - 3 / 8 * SIZE_SCREEN], undefined, undefined, get_size_px(canvas, 19), '', white)
+	canvas.addChild(description_bar)
 
 	// ------------------------------------------------ Logic and Canvas --------------------------------
 
-	frame = build_frame(canvas);
-	template = build_template(canvas);
-	actual_screen = build_main_screen(canvas);
+	frame = build_frame(canvas)
+	template = build_template(canvas)
+	actual_screen = build_main_screen(canvas)
 
-	canvas.addChild(frame);
-	canvas.addChild(actual_screen);
-	canvas.addChild(description_bar);
+	canvas.addChild(frame)
+	canvas.addChild(actual_screen)
+	canvas.addChild(description_bar)
 
 	canvas.setLoop(function() {
-		var d = new Date();
-		counter = (counter + 1) % fps;
+		var d = new Date()
+		counter = (counter + 1) % fps
 
 		switch (actual_screen.description) {
 			case descriptions['main']:
@@ -76,102 +62,102 @@ function loadCanvas() {
 					('0' + (d.getMonth() + 1)).slice(-2) +
 					' / ' +
 					d.getFullYear() +
-					'\n';
-				actual_screen.time.text = ('0' + d.getHours()).slice(-2) + ':' + ('0' + d.getMinutes()).slice(-2);
-				break;
+					'\n'
+				actual_screen.time.text = ('0' + d.getHours()).slice(-2) + ':' + ('0' + d.getMinutes()).slice(-2)
+				break
 
 			case descriptions['pattern_lock']:
 				if (canvas.mouse.buttonState == 'down' || canvas.touch.touchState == 'down')
-					actual_screen.active = true;
+					actual_screen.active = true
 				else if (canvas.mouse.buttonState == 'up' || canvas.touch.touchState == 'up') {
-					changeScreen(canvas, build_lock_screen(canvas));
+					changeScreen(canvas, build_lock_screen(canvas))
 				}
 				
-				if (actual_screen.attempt == code) changeScreen(canvas, build_menu_screen(canvas));
-				break;
+				if (actual_screen.attempt == code) changeScreen(canvas, build_menu_screen(canvas))
+				break
 
 			case descriptions['fingerprint_lock']:
-				actual_screen.progress_circle_fingerprint.rotation++;
-				actual_screen.progress_circle_fingerprint.end += actual_screen.progress_circle_fingerprint.touching;
+				actual_screen.progress_circle_fingerprint.rotation++
+				actual_screen.progress_circle_fingerprint.end += actual_screen.progress_circle_fingerprint.touching
 
 				if (actual_screen.progress_circle_fingerprint.end >= 360) {
-					actual_screen.progress_circle_fingerprint.touching = 0;
-					actual_screen.progress_circle_fingerprint.end = 0;
-					changeScreen(canvas, build_menu_screen(canvas));
+					actual_screen.progress_circle_fingerprint.touching = 0
+					actual_screen.progress_circle_fingerprint.end = 0
+					changeScreen(canvas, build_menu_screen(canvas))
 				}
 
-				break;
+				break
 
 			case descriptions['new_measurement']:
-				actual_screen.circle.rotation++;
-				actual_screen.circle.end += 2;
+				actual_screen.circle.rotation++
+				actual_screen.circle.end += 2
 
 				if (actual_screen.circle.end >= 360 && !actual_screen.done) {
-					actual_screen.circle.stroke = '10px #55AA55';
-					actual_screen.done = true;
-					actual_screen.addChild(actual_screen.done_text);
+					actual_screen.circle.stroke = '10px #55AA55'
+					actual_screen.done = true
+					actual_screen.addChild(actual_screen.done_text)
 
 					switch (actual_screen.back_page) {
 						case 'blood_pressure':
 							health_information.today.systolic =
-								MIN_SYSTOLIC_DAY + Math.floor(Math.random() * (MAX_SYSTOLIC_DAY - MIN_SYSTOLIC_DAY));
+								MIN_SYSTOLIC_DAY + Math.floor(Math.random() * (MAX_SYSTOLIC_DAY - MIN_SYSTOLIC_DAY))
 							health_information.today.diastolic =
-								MIN_DIASTOLIC_DAY + Math.floor(Math.random() * (MAX_DIASTOLIC_DAY - MIN_DIASTOLIC_DAY));
-							break;
+								MIN_DIASTOLIC_DAY + Math.floor(Math.random() * (MAX_DIASTOLIC_DAY - MIN_DIASTOLIC_DAY))
+							break
 						case 'blood_oxygen':
-							health_information.today.blood_oxygen = Math.random() / 2 + 0.5;
-							break;
+							health_information.today.blood_oxygen = Math.random() / 2 + 0.5
+							break
 					}
 				}
 
-				break;
+				break
 
 			case descriptions['sos']:
 				if (!sos.active && (canvas.mouse.buttonState == 'up' && canvas.touch.touchState == 'up')) {
-					actual_screen.message_hold.text = health['press_3_seconds'];
+					actual_screen.message_hold.text = health['press_3_seconds']
 				} else if (sos.active && (canvas.mouse.buttonState == 'up' && canvas.touch.touchState == 'up')) {
-					actual_screen.message_hold.text = health['press_5_seconds_cancel'];
+					actual_screen.message_hold.text = health['press_5_seconds_cancel']
 				}
 
 			case descriptions['map']:
 				if (clicked) {
-					var map_html = document.getElementById('mapid');
-					map_html.style.display = 'none';
-					map_initialized.remove();
+					var map_html = document.getElementById('mapid')
+					map_html.style.display = 'none'
+					map_initialized.remove()
 					
-					changeScreen(canvas, build_places_list_screen(canvas));
+					changeScreen(canvas, build_places_list_screen(canvas))
 				}
-				break;
+				break
 		}
 
 		if (fitness.started) {
 			if (fitness.type == 'activity_walk') {
 				if (counter == 0) {
-					fitness.distance += 0.001;
-					fitness.calories += 5;
-					fitness.steps += 1;
-					fitness.duration += 1 / 60;
+					fitness.distance += 0.001
+					fitness.calories += 5
+					fitness.steps += 1
+					fitness.duration += 1 / 60
 				}
 			} else if (fitness.type == 'activity_run') {
 				if (counter == 0) {
-					fitness.distance += 0.002;
-					fitness.calories += 8;
-					fitness.steps += 4;
-					fitness.duration += 1 / 60;
+					fitness.distance += 0.002
+					fitness.calories += 8
+					fitness.steps += 4
+					fitness.duration += 1 / 60
 				}
 			} else if (fitness.type == 'activity_gym') {
 				if (counter == 0) {
-					fitness.distance += 0.001;
-					fitness.calories += 10;
-					fitness.steps += 6;
-					fitness.duration += 1 / 60;
+					fitness.distance += 0.001
+					fitness.calories += 10
+					fitness.steps += 6
+					fitness.duration += 1 / 60
 				}
 			} else if (fitness.type == 'activity_bike') {
 				if (counter == 0) {
-					fitness.distance += 0.005;
-					fitness.calories += 10;
-					fitness.steps += 0;
-					fitness.duration += 1 / 60;
+					fitness.distance += 0.005
+					fitness.calories += 10
+					fitness.steps += 0
+					fitness.duration += 1 / 60
 				}
 			}
 
@@ -180,24 +166,22 @@ function loadCanvas() {
 				descriptions['activity_run'],
 				descriptions['activity_gym'],
 				descriptions['activity_bike']
-			];
+			]
+
 			if (counter == 0 && descriptions_valid.includes(actual_screen.description))
-				changeScreen(canvas, build_start_activity_screen(canvas));
+				changeScreen(canvas, build_start_activity_screen(canvas))
 		}
 
-		if (sos.active) {
-			sos.audio_emergency.play();
-		} else {
-			sos.audio_emergency.pause();
-		}
+		if (sos.active) sos.audio_emergency.play()
+		else sos.audio_emergency.pause()
 
 		if (actual_screen.template)
-			template.time.text = ('0' + d.getHours()).slice(-2) + ':' + ('0' + d.getMinutes()).slice(-2);
+			template.time.text = ('0' + d.getHours()).slice(-2) + ':' + ('0' + d.getMinutes()).slice(-2)
 
-		if (canvas.mouse.buttonState == 'up') frame.emergency = 5;
+		if (canvas.mouse.buttonState == 'up') frame.emergency = 5
 
 		// TODO: Check if it's needed to update drawing of number of friend group
-	});
+	})
 
-	canvas.timeline.start();
+	canvas.timeline.start()
 }
